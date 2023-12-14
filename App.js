2b8a52ccd36b2c05167084f610aa3aad2224b400
ChandruSheku <150648802+ChandruSheku@users.noreply.github.com>
2023-12-14 20:50:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Todo from './ENTRIPROJECT/Todo.css';
+import React from 'react';
+import TodoList from './ENTRIPROJECT/Todolist';
+const App = () => {
+  return (
+    <div>
+            <TodoList/>
+    </div>
+  );
+};
+
+export default App;
---
+++ Todolist.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, { text: inputValue, completed: false }]);
+      setInputValue('');
+    }
+  };
+
+  const handleToggleComplete = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+    setTodos(updatedTodos);
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter a new todo"
+        />
+        <button onClick={handleAddTodo}>Add Todo</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleToggleComplete(index)}
+            />
+            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+              {todo.text}
+            </span>
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
